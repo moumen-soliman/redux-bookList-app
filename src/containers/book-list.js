97e44fa7ec615d1d9d099ee050742d4aa092e7d9
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-02 14:13:07 +0200

    Connect between redux & react

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
@@ -17,3 +18,11 @@ export default class BookList extends Component {
 		)
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		books: state.books
+	};
+}
+
+export default connect(mapStateToProps)(BookList)
