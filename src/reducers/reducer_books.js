f689d37358c61fb18bcdf218270fec1b8c8249d8
Mo'men Soliman <moumensoliman@gmail.com>
2018-02-03 19:39:55 +0200

    Add book details and import details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => { 
 	return [
-		{ title: 'Javascript: The Good Parts' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Elqouent Ruby' }
+		{ title: 'Javascript: The Good Parts', pages: '102'},
+		{ title: 'Harry Potter', pages: '12'},
+		{ title: 'The Dark Tower', pages: '107'},
+		{ title: 'Elqouent Ruby', pages: '112'}
 	];
 }
